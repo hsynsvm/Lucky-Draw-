c64b03c2dea09588f034c2893b6825dc187d810c
Hüseyin SEVİM <hsyn_svm@hotmail.com>
2023-11-18 21:46:00 +0300

    A fun lucky draw app
    A fun lucky draw app

---
+++ CekilisUygulaması/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CekilisUygulaması
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Çekilişe Hoşgeldiniz :) ");
+            Console.WriteLine("\nKaç Kişilik Çekiliş Yapılacak?");
+            int sayi = Convert.ToInt32(Console.ReadLine());
+
+            Console.Clear();
+
+            string[] katilimcilar = new string[sayi];
+
+            for (int i = 0; i < katilimcilar.Length; i++)
+            {
+                Console.WriteLine((i+1) + ". Katılımcı Adı: ");
+                string katilimci = Console.ReadLine();
+                katilimcilar[i] = katilimci;
+            }
+            Random rnd = new Random();
+            int cekilen = rnd.Next(0,sayi-1);
+            Console.WriteLine("Kazanan = " + katilimcilar[cekilen]);
+
+        }
+    }
+}
